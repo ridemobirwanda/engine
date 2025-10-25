5e9d758c27dd32d8db765b2cf24eeacb25a82967
engine <enginemarkets.com>
2025-10-25 15:09:37 +0200

    Add wrangler.json to configure Cloudflare Pages Functions correctly

--- public/_worker.js
+++ public/_worker.js
@@ -1,2 +1,4 @@
-// This file is deprecated - use functions/[[path]].js instead
+// DEPRECATED - This file should not be used
+// Cloudflare Pages Functions are in the functions/ directory instead
+// This file is kept for reference only and will be deleted
 
